d4e6d55f4d87f9b1073c3d5a6b8c7c24100634cb
johnmaster <juanpuhl404@gmail.com>
2022-10-10 23:03:02 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ejerc_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num=0, factor;
+
+            
+
+            do{
+                
+                Console.Clear();
+                Console.WriteLine("**FACTORIAL DE UN NUMERO**");
+                Console.WriteLine("0 - Salir");    
+                Console.WriteLine("Numero para Factorial: ");
+                num=Convert.ToInt32(Console.ReadLine());
+
+                if(num!=0){
+                    factor=1;
+
+                    for(int i=1; i<=num ; i++){
+                        factor=factor*i;
+
+                    }
+                Console.WriteLine("El Factorial de: " + num +" es " + factor);
+                Console.ReadKey();
+                }
+ 
+            } while(num!=0);
+        }
+    }
+}
